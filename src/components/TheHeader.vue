cd60b75ef1bc16306191c8eedf16156eb038ee70
黃琮壬 <404250348@gapp.fju.edu.tw>
2020-08-10 03:50:24 +0800

    Create a new project to develop pwa function.

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="imam-controller-nav">
+        <b-container>
+            <b-navbar
+                    toggleable="lg"
+                    type="dark"
+            >
+                <b-navbar-brand
+                        to="/"
+                >
+                    <span class="mx-1">今時科技</span>
+                </b-navbar-brand>
+                <b-navbar-toggle target="nav-collapse" />
+                <b-collapse
+                        id="nav-collapse"
+                        is-nav
+                >
+                    <b-navbar-nav>
+                        <b-nav-item
+                                to="/config"
+                                class="my-3 text-center"
+                        >
+                            Config
+                        </b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-navbar>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TheHeader",
+    }
+</script>
+
+<style scoped>
+    #imam-controller-nav {
+        background-color: #42b983;
+    }
+
+    #nav-collapse {
+        background-color: #42b983;
+        z-index: 99;
+    }
+
+    @media screen and (max-width: 991px) {
+        #nav-collapse {
+            margin: 0 -2rem;
+        }
+    }
+
+    @media screen and (min-width: 992px) {
+        #nav-collapse {
+            height: 0;
+        }
+    }
+
+    a {
+        color: white !important;
+        letter-spacing: 1px;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
+import axios from 'axios'
+import VueAxios from "vue-axios";
+
+axios.defaults.withCredentials = true
+Vue.use(BootstrapVue, VueAxios, axios)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
